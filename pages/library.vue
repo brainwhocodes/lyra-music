bd478cc32098ba14bc5ac459d8c3fc7580ad16a2
brainwhocodes <brainwhocodes@outlook.com>
2025-06-23 23:04:59 -0400

    Replace avatar dropdown with icon button

--- pages/library.vue
+++ pages/library.vue
@@ -20,18 +20,16 @@
          <button class="btn btn-ghost btn-circle">
            <Icon name="material-symbols:settings-outline" class="w-6 h-6" />
          </button>
-         <div class="dropdown dropdown-end">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-              <div class="w-10 rounded-full">
-                <img alt="User Avatar" src="" />
-              </div>
-            </div>
-            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-              <li><NuxtLink to="/settings">Settings</NuxtLink></li>
-               <li><hr class="my-1" /></li>
-              <li><a>Logout</a></li>
-            </ul>
-          </div>
+        <div class="dropdown dropdown-end">
+          <button tabindex="0" class="btn btn-ghost btn-circle" @click.stop>
+            <Icon name="material-symbols:more-vert" />
+          </button>
+          <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <li><NuxtLink to="/settings">Settings</NuxtLink></li>
+             <li><hr class="my-1" /></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
       </div>
     </div>
 
